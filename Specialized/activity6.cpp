38b49bf88eec0e93e307656af0d69fd0953c8a76
Ahmad Williamson <ahmadwilliamson@The-Hidden-Genius-Project-70.local>
2022-07-13 12:13:42 -0700

    first commit

---
+++ Specialized/activity6.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <stdio.h>
+#include <stdlib.h> 
+#include <cstdlib>
+using namespace std;
+ 
+int main() {
+int randomVal;
+ 
+srand(5);
+ 
+randomVal = rand() % 6;
+ 
+cout << randomVal;
+ 
+randomVal = rand() % 6;
+ 
+cout << randomVal;
+ 
+randomVal = rand() % 6;
+ 
+cout << randomVal;
+ 
+return 0;
+}
